f9f2829b3f0c9026cf643dc30e274d494f77ae9c
Dodecahedra <k.degeling@student.tue.nl>
2020-11-23 15:59:02 +0100

    Use ParityGame format instead of BES
    Change from BES to ParityGame to read in Parity Games.

--- tools/release/pgsolve_gjkw/pgsolve_gjkw.cpp
+++ tools/release/pgsolve_gjkw/pgsolve_gjkw.cpp
@@ -12,61 +12,71 @@
 /// [Groote/Jansen/Keiren/Wijs 2017]
 
 #include "mcrl2/utilities/input_output_tool.h"
-#include "mcrl2/bes/pbes_input_output_tool.h"
-
-#include "mcrl2/bes/parse.h"
+#include "pg.h"
+#include "pgsolver_io.h"
+#include "utilities.h"
 #include "mcrl2/lts/lts_algorithm.h"
 
 using namespace mcrl2::lts; // For reduce() function.
-using namespace mcrl2::bes; // For parsing a PG from input.
-using namespace mcrl2::utilities::tools;
 using namespace mcrl2::log;
-using mcrl2::bes::tools::bes_input_output_tool;
 
-namespace mcrl2 {
+// namespace mcrl2 {
 
-class pg_convert
-{
-  protected:
-    /** Storing bes from Parity Game */
-    boolean_equation_system m_bes;
-    /** Reduced Kripke structure in lts */
-    lts::lts_aut_t m_lts;
+// template<class LTS_TYPE>
+// class pg_convert  : public lts::detail::bisim_gjkw::bisim_partitioner_gjkw_initialise_helper
+// {
 
-  public:
-  pg_convert(boolean_equation_system& v_bes)
-  {
-    m_bes = v_bes;
-  }
+//   protected:
+//     /** Storing bes from Parity Game */
+//     boolean_equation_system m_bes;
+//     /** Reduced Kripke structure in lts */
+//     lts::lts_aut_t m_lts;
 
-  void convert_bes()
-  {
-    // Convert reduced lts to BES
-  }
-
-  void convert_lts()
-  {
-    // Convert BES to kripke structure in lts
-  }
-
-  void run()
-  {
-    mCRL2log(verbose) << "Starting conversion to LTS..." << std::endl;
-    convert_bes();
-    mCRL2log(verbose) << "Reducing LTS using m log n algorithm" << std::endl;
-    reduce(m_lts, lts_eq_divergence_preserving_branching_bisim_gjkw);
-    mCRL2log(verbose) << "Converting to BES" << std::endl;
-    convert_lts();
-  }
-};
-
-} // End mcrl2
-
-
-typedef bes_input_output_tool<input_output_tool> super;
-class pgsolve_tool: public super
-{
+//   public:
   
+//   bisim_partitioner_gjkw_initialise_helper(LTS_TYPE& l, bool const branching,
+//                                                 bool const preserve_divergence)
+//   : aut(l),
+//     nr_of_states(l.num_states()),
+//     orig_nr_of_states(l.num_states()),
+//     nr_of_transitions(l.num_transitions()),
+//     noninert_out_per_state(l.num_states(), 0),
+//     inert_out_per_state(l.num_states(), 0),
+//     noninert_in_per_state(l.num_states(), 0),
+//     inert_in_per_state(l.num_states(), 0),
+//     noninert_out_per_block(1, 0),
+//     inert_out_per_block(1, 0),
+//     states_per_block(1, l.num_states()),
+//     nr_of_nonbottom_states(0)
+//   {
+    
+//   }
+
+//   void run()
+//   {
+//     mCRL2log(verbose) << "Starting conversion to LTS..." << std::endl;
+//     convert_bes();
+//     mCRL2log(verbose) << "Reducing LTS using m log n algorithm" << std::endl;
+//     reduce(m_lts, lts_eq_divergence_preserving_branching_bisim_gjkw);
+//     mCRL2log(verbose) << "Converting to BES" << std::endl;
+//     convert_lts();
+//   }
+// };
+
+// } // End mcrl2
+
+using mcrl2::utilities::tools::input_output_tool;
+
+template<class ParityGame>
+class pgsolve_tool: public input_output_tool
+{
+  private:
+    std::ifstream m_ifstream;
+    std::ofstream m_ofstream;
+
+  protected:
+    typedef input_output_tool super;
+
   public:
 
     pgsolve_tool()
@@ -81,13 +91,15 @@ class pgsolve_tool: public super
 
     bool run()
     {
+      std::istream& is = open_input(input_filename(), m_ifstream);
+      std::ostream& os = open_output(output_filename(), m_ofstream);
       // Main program flow
-      boolean_equation_system b;
-      mCRL2log(verbose) << "Loading BES from input file..." << std::endl;
-      load_bes(b, input_filename(), bes_input_format());
+      parity_game_t pg;
+      mCRL2log(verbose) << "Loading PG from input file..." << std::endl;
+      parse_pgsolver(pg, is, timer());
       // mcrl2::pg_convert(b).run();
       mCRL2log(verbose) << "Saving output to file..." << std::endl;
-      save_bes(b, output_filename(), bes_output_format());
+      print_pgsolver(pg, os);
       mCRL2log(verbose) << "Terminating..." << std::endl;
       return true;
     }
@@ -96,5 +108,5 @@ class pgsolve_tool: public super
 
 int main(int argc, char* argv[])
 {
-  return pgsolve_tool().execute(argc, argv);
+  return pgsolve_tool<parity_game_t>().execute(argc, argv);
 }
